e6327ee762eacb29508be4e630d59e8a020bcd19
zhoubenliang <554524787@qq.com>
2020-05-08 22:32:51 +0800

    aaa

--- SharePreferencesTest/app/src/main/java/com/example/sharepreferencestest/MainActivity.kt
+++ SharePreferencesTest/app/src/main/java/com/example/sharepreferencestest/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.example.sharepreferencestest
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        saveButton.setOnClickListener {
+
+            val editor = getSharedPreferences("data", Context.MODE_PRIVATE).edit().apply() {
+                putString("name", "Tom")
+                putInt("age", 27)
+                putBoolean("married", false)
+                apply()
+            }
+
+        }
+        restoreButton.setOnClickListener {
+            val prefs = getSharedPreferences("data", Context.MODE_PRIVATE)
+            val name = prefs.getString("name", "")
+            val age = prefs.getInt("age", 0)
+            val married = prefs.getBoolean("married", false)
+            Log.d("MainActivity", "name is $name")
+            Log.d("MainActivity", "age is $age")
+            Log.d("MainActivity", "married is $married")
+        }
     }
 }

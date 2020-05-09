b5a199f77c2d9198371aac28263420e531db9bcf
zhoubenliang <554524787@qq.com>
2020-05-09 21:39:48 +0800

    aaaaa

--- NotificactionTest/app/src/main/java/com/example/notificactiontest/MainActivity.kt
+++ NotificactionTest/app/src/main/java/com/example/notificactiontest/MainActivity.kt
@@ -1,12 +1,54 @@
 package com.example.notificactiontest
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel =
+                NotificationChannel("normal", "Normal", NotificationManager.IMPORTANCE_DEFAULT)
+            val channel2 =
+                NotificationChannel("important", "Important", NotificationManager.IMPORTANCE_HIGH)
+            manager.createNotificationChannel(channel2)
+        }
+        sendNotice.setOnClickListener {
+            val intent = Intent(this, NotificationActivity::class.java)
+            val pi = PendingIntent.getActivity(this, 0, intent, 0)
+            val notification = NotificationCompat.Builder(this, "important")
+                .setContentTitle("This is content title")
+                .setContentText("This is content text ...................dfsddfsdfsdfsd")
+                .setSmallIcon(R.drawable.small_icon)
+                .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.large_icon))
+                .setContentIntent(pi)
+               /* .setStyle(
+                    NotificationCompat.BigTextStyle().bigText(
+                        "learn how to build notifications ,send and sync data and use voice actions. Get " +
+                                "the official Android IDE and developer tools to build apps for Android"
+                    )
+                )*/
+                 .setStyle(
+                     NotificationCompat.BigPictureStyle()
+                         .bigLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.big_image))
+                 )
+                .setAutoCancel(true)
+                .build()
+            manager.notify(1, notification)
+        }
+
     }
 }

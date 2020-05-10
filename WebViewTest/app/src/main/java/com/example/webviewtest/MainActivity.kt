f3c1cac6363195e45cf5b20f2229afb5051431a5
zhoubenliang <554524787@qq.com>
2020-05-10 16:33:27 +0800

    aaaaaaaaaaaaaa

---
+++ WebViewTest/app/src/main/java/com/example/webviewtest/MainActivity.kt
@@ -0,0 +1,124 @@
+package com.example.webviewtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.xmlpull.v1.XmlPullParser
+import org.xmlpull.v1.XmlPullParserFactory
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.io.StringReader
+import java.lang.Exception
+import java.lang.StringBuilder
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        /* webView.settings.javaScriptEnabled = true
+         webView.webChromeClient= WebChromeClient()
+         webView.loadUrl("https://v.qq.com")*/
+        sendRequestBtn.setOnClickListener {
+            //sendRequestWithHttpURLConnection()
+            sendRequestWithOkhttp()
+        }
+
+    }
+
+    private fun sendRequestWithOkhttp() {
+        thread {
+            try {
+                val client = OkHttpClient()
+                val request = Request.Builder()
+                    .url("http://10.0.2.2/get_data.xml")
+                    .build()
+                val response = client.newCall(request).execute()
+                val responseData = response.body?.string()
+                if (responseData != null) {
+                    //showResponse(responseData)
+                    parseXMLWithPull(responseData)
+                }
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun parseXMLWithPull(responseData: String) {
+        try {
+            val factory = XmlPullParserFactory.newInstance()
+            val xmlPullParser = factory.newPullParser()
+            xmlPullParser.setInput(StringReader(responseData))
+            var eventType = xmlPullParser.eventType
+            var id = ""
+            var name = ""
+            var version = ""
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                val nodeName = xmlPullParser.name
+                when (eventType) {
+                    //开始解析某个节点
+                    XmlPullParser.START_TAG -> {
+                        when (nodeName) {
+                            "id" -> id = xmlPullParser.nextText()
+                            "name" -> name = xmlPullParser.nextText()
+                            "version" -> version = xmlPullParser.nextText()
+                        }
+                    }
+                    //开始完成解析某个节点
+                    XmlPullParser.END_TAG -> {
+                        if ("app" == nodeName) {
+                            Log.d("MainActivity", "id is $id")
+                            Log.d("MainActivity", "name is $name")
+                            Log.d("MainActivity", "version is $version")
+                        }
+                    }
+                }
+                eventType = xmlPullParser.next()
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun sendRequestWithHttpURLConnection() {
+
+        thread {
+            var connection: HttpURLConnection? = null
+            try {
+                val response = StringBuilder()
+                val url = URL("https://www.baidu.com")
+                connection = url.openConnection() as HttpURLConnection
+                connection.requestMethod = "GET"
+                connection.connectTimeout = 8000
+                connection.readTimeout = 8000
+                val input = connection.inputStream
+                val reader = BufferedReader(InputStreamReader(input))
+                reader.use {
+                    reader.forEachLine { response.append(it) }
+                }
+                showResponse(response.toString())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            } finally {
+                connection?.disconnect()
+            }
+
+
+        }
+    }
+
+    private fun showResponse(response: String) {
+        runOnUiThread {
+            responseText.text = response
+        }
+    }
+}
